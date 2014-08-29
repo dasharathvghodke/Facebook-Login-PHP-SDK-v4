6958bbb48f25dcbfafc8e6c817c8170cc34a0f9e
ssgaur <shailendra.singh.knp@gmail.com>
2014-08-30 03:03:10 +0530

    version 1.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	session_start();
+	require_once 'handle.php';
+/*
+	here we check that user is already logged in with facebook or not; 
+	if yes than show user's info
+*/
+  	if (isset($_SESSION['facebook_token'])) {
+    	print_r($graph);
+    	echo '<br><a href="index.php?logout_From_Facebook"> Logout from Facebook</a>';
+  	}
+  	else{
+    	echo '<a href="'.$loginUrl.'">Login with Facebook</a>';
+  	}
+?>
